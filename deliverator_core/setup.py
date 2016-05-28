1a87e1b52e9754b9b2af5b0c2e87a36fca962628
juztamau5 <juztamau5@gmail.com>
2016-05-27 18:29:34 -0700

    [core] Fix installation of python module

--- deliverator_core/setup.py
+++ deliverator_core/setup.py
@@ -28,7 +28,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-  packages=['deliverator'],
+  packages=['deliverator', 'deliverator.diagnostics'],
   package_dir={'': 'modules'},
 )
 
