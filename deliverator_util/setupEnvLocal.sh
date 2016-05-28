952c4995ddfac0caa84c07331c9874e2100d2b9d
juztamau5 <juztamau5@gmail.com>
2016-05-28 15:05:16 -0700

    [util] Remove setupEnvLocal.sh and setupEnvRemote.sh

--- deliverator_util/setupEnvLocal.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# source this when you launch everything on a local machine
-export ROS_MASTER_URI=http://localhost:11311
-export MASTER_HOSTNAME=localhost
-export HOSTNAME=localhost
-export ROSLAUNCH_SSH_UNKNOWN=0
