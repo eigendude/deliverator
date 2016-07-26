d594eff3de44fc1479fda5f87dd015737bf6b385
juztamau5 <juztamau5@gmail.com>
2016-07-25 19:48:48 -0700

    [cosmetic] Whitespace indentation in setup_env.sh

--- deliverator_util/setup_env.sh
+++ deliverator_util/setup_env.sh
@@ -7,11 +7,11 @@ for ros_distro in kinetic \
                   jade \
                   indigo \
 ; do
-	SETUP_SCRIPT="/opt/ros/$ros_distro/setup.bash"
-	if [ -f $SETUP_SCRIPT ]; then
-		source $SETUP_SCRIPT
-		break
-	fi
+    SETUP_SCRIPT="/opt/ros/$ros_distro/setup.bash"
+    if [ -f $SETUP_SCRIPT ]; then
+        source $SETUP_SCRIPT
+        break
+    fi
 done
 
 # Source project script
