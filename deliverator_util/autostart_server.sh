b888beb72bfb63785bc3cb19e8512b0c0b6b495b
juztamau5 <juztamau5@gmail.com>
2016-07-25 18:46:09 -0700

    [util] autostart_server.sh: Fix relative file

--- deliverator_util/autostart_server.sh
+++ deliverator_util/autostart_server.sh
@@ -3,6 +3,8 @@
 # Add to /etc/rc.local:
 #     su <username> -c "<path_to_deliverator_util>/autostart_server.sh"
 
-source setup_env.sh
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+source $SCRIPT_DIR/setup_env.sh
 
 roslaunch deliverator_core autostart_server.launch
